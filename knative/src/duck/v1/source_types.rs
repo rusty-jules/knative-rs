9a02f19082ad4296619a71f470fe3406b76eaffe
Julian Aichholz <julianaichholz@gmail.com>
2022-07-25 21:55:45 -0700

    feat: SourceManager trait for specifying and managing Conditions
    - Make ConditionType extension with Extension variant
    - impl Default Condition

--- knative/src/duck/v1/source_types.rs
+++ knative/src/duck/v1/source_types.rs
@@ -2,7 +2,7 @@
 use super::{
     knative_reference::KReference,
     status_types::{
-        ConditionStatus, ConditionType, Status,
+        ConditionType, ConditionManager, Conditions, Condition, Status,
     },
 };
 use crate::error::{DiscoveryError, Error};
@@ -117,55 +117,134 @@ pub struct SourceStatus {
     pub cloud_event_attributes: Option<Vec<CloudEventAttributes>>,
 }
 
-#[allow(unreachable_patterns)]
-impl SourceStatus {
-    // returns true if the resource is ready overall.
-    pub fn is_ready(&self) -> bool {
-        match &self.status.conditions {
-            Some(conditions) => conditions.iter().any(|c| match c.type_ {
-                // Look for the "happy" condition, which is the only condition that
-                // we can reliably understand to be the overall state of the resource.
-                ConditionType::Ready | ConditionType::Succeeded => {
-                    c.status == ConditionStatus::True
-                }
-                _ => false,
-            }),
-            None => false,
-        }
+/// Allows a source status CR to manage its Conditions
+pub trait SourceManager<'de, const N: usize> {
+    /// The [`ConditionTypes`] that the `Ready` status of the source depends on.
+    ///
+    /// [`ConditionTypes`]: ../status_types/enum.ConditionType.html
+    type Dependents: Deserialize<'de> + Serialize + Clone + JsonSchema + ToString;
+
+    /// Return the dependent ConditionTypes. Can be defined in a number of ways:
+    /// ### As &'static str
+    ///
+    /// ```rust
+    /// impl<'de> SourceManager<'de, 1> for MyStatus {
+    ///     type Dependents = &'static str;
+    ///
+    ///     fn dependents() -> [Self::Dependents; 1] {
+    ///         ["SinkProvided"]
+    ///     }
+    ///
+    ///     ...
+    /// }
+    /// ```
+    ///
+    /// ### As an Enum
+    ///
+    /// ```rust
+    /// use schemars::JsonSchema;
+    /// use serde::{Serialize, Deserialize};
+    /// use std::fmt;
+    ///
+    /// #[derive(Deserialize, Serialize, Clone, JsonSchema)]
+    /// enum MyConditions {
+    ///     SinkProvided,
+    ///     VeryImportant,
+    ///     NotImportant
+    /// }
+    ///
+    /// impl fmt::Display for MyConditions {
+    ///     fn fmt<'a>(&self, fmt: &mut fmt::Formatter<'a>) -> fmt::Result {
+    ///         std::write!(fmt, "{:?}", self)
+    ///     }
+    /// }
+    ///
+    /// impl<'de> SourceManager<'de, 2> for MyStatus {
+    ///     type Dependents = MyConditions;
+    ///
+    ///     fn dependents() -> [Self::Dependents; 2] {
+    ///         [MyConditions::SinkProvided, MyConditions::VeryImportant]
+    ///     }
+    ///
+    ///     ...
+    /// }
+    /// ```
+    fn dependents() -> [Self::Dependents; N];
+
+    /// Return the conditions of your CRD Status type.
+    fn conditions(&mut self) -> &mut Conditions;
+
+    /// Return the SourceStatus of your CRD Status type.
+    fn source_status(&mut self) -> &mut SourceStatus;
+
+    /// Construct a [`ConditionManager`] of your dependent Conditions and the
+    /// `Ready` or `Succeeded` status.
+    ///
+    /// [`ConditionManager`]: ../status_types/struct.ConditionManager.html
+    fn manager(&mut self) -> ConditionManager<N> {
+        ConditionManager::new_living(
+            Self::dependents().map(|d| ConditionType::Extension(d.to_string())),
+            self.conditions()
+        )
     }
 
-    pub fn mark_ready(&mut self) {
-        if let Some(ref mut cond) = &mut self.status.conditions {
-            cond.mark_true(ConditionType::Ready)
-        }
-    }
-
-    pub fn mark_not_ready(&mut self, reason: &str, message: String) {
-        if let Some(ref mut cond) = &mut self.status.conditions {
-            cond.mark_false(ConditionType::Ready, reason.to_string(), Some(message))
-        }
-    }
-
-    pub fn mark_unknown(&mut self) {
-        if let Some(ref mut cond) = &mut self.status.conditions {
-            cond.mark_unknown(ConditionType::Ready)
-        }
+    /// Returns true if the resource is ready overall.
+    fn is_ready(&mut self) -> bool {
+        self.manager().is_happy()
     }
 
     /// Set the condition that the source has a sink configured
-    pub fn mark_sink(&mut self, uri: url::Url) {
-        self.sink_uri = Some(uri);
-        if let Some(ref mut cond) = &mut self.status.conditions {
-            cond.mark_true(ConditionType::SinkProvided)
-        }
+    fn mark_sink(&mut self, uri: url::Url) {
+        self.source_status().sink_uri = Some(uri);
+        self.manager().mark_true(&ConditionType::sinkprovided());
     }
 
     /// Set the condition that the source has no sink configured
-    pub fn mark_no_sink(&mut self, reason: &str, message: String) {
-        self.sink_uri = None;
-        if let Some(ref mut cond) = &mut self.status.conditions {
-            cond.mark_false(ConditionType::SinkProvided, reason.to_string(), Some(message))
+    fn mark_no_sink(&mut self, reason: &str, message: Option<String>) {
+        self.source_status().sink_uri = None;
+        self.manager().mark_false(&ConditionType::sinkprovided(), reason, message);
+    }
+
+    /// Set the condition that the source status is unknown. Typically used when beginning the
+    /// reconciliation of a new generation.
+    fn mark_unknown(&mut self) {
+        let mut cm = self.manager();
+        let type_ = &cm.get_top_level_condition().type_.clone();
+        cm.mark_unknown(
+            type_,
+            "NewObservedGenFailure",
+            Some("unsuccessfully observed a new generation".into())
+        );
+    }
+}
+
+#[allow(unreachable_patterns)]
+impl<'de> SourceManager<'de, 1> for SourceStatus {
+    type Dependents = ConditionType;
+
+    fn dependents() -> [ConditionType; 1] {
+        [ConditionType::sinkprovided()]
+    }
+
+    fn conditions(&mut self) -> &mut Conditions {
+        match self.status.conditions {
+            Some(ref mut conditions) => conditions,
+            None => {
+                self.status.conditions = Some(Conditions::with_conditions(
+                    vec![
+                        Condition::default(),
+                        Condition {
+                            type_: ConditionType::sinkprovided(),
+                            ..Default::default()
+                        },
+                    ]));
+                self.conditions()
+            }
         }
+    }
+
+    fn source_status(&mut self) -> &mut SourceStatus {
+        self
     }
 }
 
