d8736cd395401dc5f9b4fb76e79e06c8e84fbc92
Julian Aichholz <julianaichholz@gmail.com>
2022-11-01 22:50:47 -0700

    feat: build_service_url
    Use a shared fn build_service_url for both DynamicObject and Service
    types when getting the url via AddressableTypeExt.

--- knative/src/duck/v1/addressable_type.rs
+++ knative/src/duck/v1/addressable_type.rs
@@ -1,7 +1,7 @@
 use k8s_openapi::api::core::v1::Service;
 use kube::Config;
 use kube::api::DynamicObject;
-use kube::api::{Resource, ResourceExt, ApiResource};
+use kube::api::{Resource, ResourceExt};
 use thiserror::Error;
 use url::Url;
 use serde_json::Value;
@@ -40,54 +40,15 @@ pub struct AddressableType {
     pub status: AddressableStatus
 }
 
-impl TryFrom<Service> for AddressableType {
-    type Error = AddressableErr;
+#[doc(hidden)]
+/// Construct the uri from the service metadata
+async fn build_service_url(name: &str, namespace: &str) -> Result<Url, AddressableErr> {
+    let cluster_url = Config::infer().await?.cluster_url;
+    let scheme = cluster_url.scheme().unwrap_or(&http::uri::Scheme::HTTP);
+    let cluster_host = cluster_url.host().unwrap_or("cluster.local");
+    let url = Url::parse(&format!("{scheme}://{name}.{namespace}.svc.{cluster_host}"))?;
 
-    fn try_from(service: Service) -> Result<Self, Self::Error> {
-        let name = service.name();
-        let namespace = service.namespace().unwrap_or("default".into());
-        let cluster_url = {
-            // Copied straight from kube_client::config::file_loader to avoid async
-            // params, though it only supports local kubernetes config file
-            let config = kube::config::Kubeconfig::read()?;
-            let context_name = match &config.current_context {
-                Some(name) => name,
-                None => Err(kube::config::KubeconfigError::CurrentContextNotSet)?
-            };
-            let current_context = config
-                .contexts
-                .iter()
-                .find(|named_context| &named_context.name == context_name)
-                .map(|named_context| &named_context.context)
-                .ok_or_else(|| kube::config::KubeconfigError::LoadContext(context_name.clone()))?;
-            let cluster_name = &current_context.cluster;
-            let cluster = config
-                .clusters
-                .iter()
-                .find(|named_cluster| &named_cluster.name == cluster_name)
-                .map(|named_cluster| &named_cluster.cluster)
-                .ok_or_else(|| kube::config::KubeconfigError::LoadClusterOfContext(cluster_name.clone()))?;
-            let cluster_url = cluster
-                .server
-                .parse::<http::Uri>()
-                .map_err(kube::config::KubeconfigError::ParseClusterUrl)?;
-            cluster_url
-        };
-        let scheme = cluster_url.scheme().unwrap_or(&http::uri::Scheme::HTTP);
-        let cluster_host = cluster_url.host().unwrap_or("cluster.local");
-        // Construct the uri from the service metadata
-        let url = Url::parse(
-            &format!("{scheme}://{name}.{namespace}.svc.{cluster_host}")
-        ).expect("valid url from service and config");
-
-        Ok(AddressableType {
-            status: AddressableStatus {
-                address: Addressable {
-                    url: Some(url)
-                }
-            }
-        })
-    }
+    Ok(url)
 }
 
 #[doc(hidden)]
@@ -96,10 +57,9 @@ fn parse_url_from_obj_data(name: &str, kind: &str, data: &Value) -> Result<Url, 
     if let Some(data) = data.as_object() {
         if let Some(status) = data.get("status").and_then(Value::as_object) {
             if let Some(address) = status.get("address").and_then(Value::as_object) {
-                match address.get("url").and_then(Value::as_str).map(Url::parse) {
-                    Some(Ok(url)) => return Ok(url),
-                    Some(Err(e)) => return Err(AddressableErr::UrlParseErr(e)),
-                    None => return Err(AddressableErr::UrlNotSet(name.to_string()))
+                return match address.get("url").and_then(Value::as_str).map(Url::parse) {
+                    Some(url) => Ok(url?),
+                    None => Err(AddressableErr::UrlNotSet(name.to_string()))
                 }
             }
         }
@@ -113,22 +73,23 @@ pub trait AddressableTypeExt {
 }
 
 #[async_trait::async_trait]
+impl AddressableTypeExt for AddressableType {
+    async fn try_get_address(&self) -> Result<Url, AddressableErr> {
+        self.status.address.url
+            .clone()
+            .ok_or_else(|| AddressableErr::UrlNotSet("addressable".to_string()))
+    }
+}
+
+#[async_trait::async_trait]
 impl AddressableTypeExt for DynamicObject {
     async fn try_get_address(&self) -> Result<Url, AddressableErr> {
         let name = self.meta().name.as_ref().ok_or(AddressableErr::ServiceMustHaveName)?;
-        let namespace = self.namespace().unwrap_or("default".into());
+        let namespace = self.namespace().unwrap_or_else(|| "default".into());
+
         match &self.types {
             Some(t) => match (t.api_version.as_ref(), t.kind.as_ref()) {
-                ("v1", "Service") => {
-                    let cluster_url = Config::infer().await?.cluster_url;
-                    let scheme = cluster_url.scheme().unwrap_or(&http::uri::Scheme::HTTP);
-                    let cluster_host = cluster_url.host().unwrap_or("cluster.local");
-                    // Construct the uri from the service metadata
-                    let url = Url::parse(
-                        &format!("{scheme}://{name}.{namespace}.svc.{cluster_host}")
-                    ).expect("valid url from service and config");
-                    Ok(url)
-                }
+                ("v1", "Service") => build_service_url(name, &namespace).await,
                 _ => parse_url_from_obj_data(name, t.kind.as_ref(), &self.data)
             }
             None => Err(AddressableErr::NotAddressable(name.to_string(), "unknown".to_string()))
@@ -139,12 +100,9 @@ impl AddressableTypeExt for DynamicObject {
 #[async_trait::async_trait]
 impl AddressableTypeExt for Service {
     async fn try_get_address(&self) -> Result<Url, AddressableErr> {
-        let mut dyn_obj = DynamicObject::new(
-            self.meta().name.as_ref().ok_or(AddressableErr::ServiceMustHaveName)?,
-            &ApiResource::erase::<Service>(&())
-        );
-        dyn_obj.meta_mut().namespace = self.namespace();
-        dyn_obj.try_get_address().await
+        let name = self.meta().name.as_ref().ok_or(AddressableErr::ServiceMustHaveName)?;
+        let namespace = self.namespace().unwrap_or_else(|| "default".into());
+        build_service_url(name, &namespace).await
     }
 }
 
@@ -174,9 +132,20 @@ mod test {
 
     #[async_std::test]
     async fn broker_uri() {
-        setup_kubeconfig();
         let broker = read_mock::<DynamicObject>("default_broker.yaml");
         let uri = broker.try_get_address().await.expect("broker is addressable");
+        assert_eq!(uri.scheme(), "http");
+        assert_eq!(uri.host().unwrap().to_string(), "broker-ingress.default.svc.cluster.local");
+        assert_eq!(uri.path(), "/default/default");
+    }
+
+    #[async_std::test]
+    async fn broker_status_deserializes_into_addressable() {
+        let broker = read_mock::<DynamicObject>("default_broker.yaml");
+        let addressable: AddressableType = serde_json::from_value(broker.data)
+            .expect("broker status deserializes into AddressableType");
+        let uri = addressable.try_get_address().await
+            .expect("url set on default broker");
         assert_eq!(uri.scheme(), "http");
         assert_eq!(uri.host().unwrap().to_string(), "broker-ingress.default.svc.cluster.local");
         assert_eq!(uri.path(), "/default/default");
@@ -193,12 +162,10 @@ mod test {
     }
 
     #[async_std::test]
-    async fn struct_service_uri() {
+    async fn service_struct_uri() {
         setup_kubeconfig();
-        let service: k8s_openapi::api::core::v1::Service = read_mock("default_service.yaml");
+        let service = read_mock::<Service>("default_service.yaml");
         let uri = service.try_get_address().await.expect("");
-        //let addressable: AddressableType = service.try_into().unwrap();
-        //let uri = addressable.status.address.url.unwrap();
         assert_eq!(uri.scheme(), "http");
         assert_eq!(uri.host().unwrap().to_string(), "default.default.svc.cluster.local");
         assert_eq!(uri.path(), "/");
